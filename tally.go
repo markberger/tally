185765d4411635f96878f306aafab069b19225cf
Mark Berger <mark.berger.j@gmail.com>
2013-07-22 20:16:51 -0400

    Initial commit

---
+++ tally.go
@@ -0,0 +1,222 @@
+package tally
+
+import (
+	"bufio"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net"
+	"net/http"
+	"net/textproto"
+	"os"
+	"os/signal"
+	"reflect"
+	"regexp"
+	"strings"
+)
+
+// The Action struct represents a possible reaction to a given line.
+// Regexps are attached to the Action so that they can be compiled once
+// when the bot is initialized instead of each time a line needs to be parsed.
+
+type Action struct {
+	re    *regexp.Regexp
+	Parse func(*regexp.Regexp, string) interface{}
+	Run   func(*Bot, interface{})
+}
+
+type Bot struct {
+	Server   string
+	Port     string
+	Nick     string
+	Channel  string
+	Trac_URL string
+	Ignore   []string
+	conn     net.Conn
+
+	actions []*Action
+}
+
+// Returns a new bot that has been configured according to
+// config.json
+
+func NewBot() *Bot {
+	bot := new(Bot)
+	f, err := ioutil.ReadFile("./config.json")
+	if err != nil {
+		log.Fatal("Error loading config.json: %v\n\n", err)
+	}
+	err = json.Unmarshal(f, bot)
+	if err != nil {
+		log.Fatal("Error unmarshalling config.json: %v\n\n", err)
+	}
+	return bot
+}
+
+// Establishes a connection to the server and joins a channel
+
+func (bot *Bot) Connect() {
+	dest := bot.Server + ":" + bot.Port
+	conn, err := net.Dial("tcp", dest)
+	if err != nil {
+		log.Fatalf("Unable to connect to %s\nError: %v\n\n", dest, err)
+	}
+	log.Printf("Successfully connected to %s\n", dest)
+	bot.conn = conn
+	bot.Send("USER " + bot.Nick + " 8 * :" + bot.Nick + "\n")
+	bot.Send("NICK " + bot.Nick + "\n")
+	bot.Send("JOIN " + bot.Channel + "\n")
+}
+
+// Sends a string to the server. Strings should end with a
+// newline character.
+
+func (bot *Bot) Send(str string) {
+	msg := []byte(str)
+	_, err := bot.conn.Write(msg)
+	if err != nil {
+		log.Printf("Error sending: %s", msg)
+		log.Printf("Error: %v", err)
+	} else {
+		log.Printf("Successfully sent: %s", msg)
+	}
+}
+
+// Sends a message to the channel
+
+func (bot *Bot) MsgChannel(line string) {
+	bot.Send("PRIVMSG " + bot.Channel + " :" + line + "\n")
+}
+
+func (bot *Bot) parse(line string) {
+	for i := range bot.Ignore {
+		if strings.Contains(line, bot.Ignore[i]) {
+			return
+		}
+	}
+	for i := range bot.actions {
+		action := bot.actions[i]
+		resp := action.Parse(action.re, line)
+		if resp != nil {
+			go action.Run(bot, resp)
+		}
+	}
+}
+
+func (bot *Bot) AddAction(re string, parse func(*regexp.Regexp, string) interface{},
+	run func(*Bot, interface{})) {
+	action := new(Action)
+	action.re = regexp.MustCompile(re)
+	action.Parse = parse
+	action.Run = run
+	bot.actions = append(bot.actions, action)
+}
+
+func signalHandling(bot *Bot) {
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	for sig := range c {
+		if sig == os.Interrupt {
+			log.Printf("Bot received os.Interrupt, exiting normally.\n\n")
+			bot.Send("QUIT :\n")
+			bot.conn.Close()
+			os.Exit(0)
+		}
+	}
+}
+
+func (bot *Bot) Run() {
+	go signalHandling(bot)
+	bot.SetActions()
+	reader := bufio.NewReader(bot.conn)
+	tp := textproto.NewReader(reader)
+	for {
+		line, error := tp.ReadLine()
+		if error != nil {
+			log.Printf("Error reading line: %s\n", line)
+		} else {
+			bot.parse(line)
+		}
+	}
+}
+
+func ParseTicket(re *regexp.Regexp, line string) interface{} {
+	//Regex: #(\d+)([:alpha:])*
+	matches := re.FindAllStringSubmatch(line, 10)
+	var ticket_nums []string
+	for i := range matches {
+		if matches[i][2] == "" {
+			ticket_nums = append(ticket_nums, matches[i][1])
+		}
+	}
+	if len(ticket_nums) == 0 {
+		return nil
+	}
+	return ticket_nums
+}
+
+func FetchTickets(bot *Bot, tickets interface{}) {
+	url := bot.Trac_URL + "ticket/"
+	used := make(map[string]bool)
+	ticket_nums := reflect.ValueOf(tickets)
+	for i := 0; i < ticket_nums.Len(); i++ {
+		num := ticket_nums.Index(i).String()
+		if used[num] {
+			continue
+		} else {
+			used[num] = true
+		}
+		dest := url + num
+		resp, err := http.Get(dest)
+		if err != nil {
+			fmt.Printf("%v\n", err)
+		}
+		defer resp.Body.Close()
+		body, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			fmt.Printf("%v\n", err)
+		}
+		s := string(body)
+		if strings.Contains(s, "<h1>Error:") {
+
+		} else {
+			s = s[:300]
+			a := strings.Split(s, "\n")
+			title := strings.TrimLeft(a[7], " ")
+			bot.MsgChannel(title)
+			bot.MsgChannel(dest)
+		}
+	}
+}
+
+func ParsePing(re *regexp.Regexp, line string) interface{} {
+	//Regex: ^PING
+	str := re.FindString(line)
+	if str == "" {
+		return nil
+	}
+	return line
+}
+
+func SendPong(bot *Bot, str interface{}) {
+	line := reflect.ValueOf(str).String()
+	log.Printf("Received: %s\n", line)
+	strs := strings.Split(line, " ")
+	bot.Send("PONG " + strs[1] + "\n")
+}
+
+func (bot *Bot) SetActions() {
+	if bot.Trac_URL != "" {
+		bot.AddAction(`#(\d+)([:alpha:])*`, ParseTicket, FetchTickets)
+	}
+	bot.AddAction(`^PING`, ParsePing, SendPong)
+}
+
+func InitLogging() {
+	logf, err := os.OpenFile("bot_log.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0667)
+	if err != nil {
+		log.Fatal("Cannot open/create log file.\nError: %v\n\n", err)
+	}
+	log.SetOutput(logf)
+}
---
+++ tally_test.go
@@ -0,0 +1,180 @@
+package tally
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+	"net/textproto"
+	"testing"
+)
+
+type server struct {
+	recv    []string
+	to_send string
+	addr    string
+}
+
+func mockServer(addr string, to_send string) *server {
+	s := new(server)
+	s.to_send = to_send
+	s.addr = addr
+	return s
+}
+
+func (bot *Bot) mockConnect(b chan string) {
+	dest := bot.Server + ":" + bot.Port
+	// Block until the server is ready to accept connections.
+	<-b
+	conn, err := net.Dial("tcp", dest)
+	if err != nil {
+		log.Fatalf("Unable to connect to %s\n%v\n\n", dest, err)
+	}
+	log.Printf("Successfully connected to %s\n", dest)
+	bot.conn = conn
+	// Block until the server is ready to accept data.
+	<-b
+	bot.Send("USER " + bot.Nick + " 8 * :" + bot.Nick + "\n")
+	bot.Send("NICK " + bot.Nick + "\n")
+	bot.Send("JOIN " + bot.Channel + "\n")
+}
+
+func (s *server) handle(conn net.Conn, b chan string) {
+	defer conn.Close()
+	reader := bufio.NewReader(conn)
+	tp := textproto.NewReader(reader)
+	b <- ""
+	for {
+		line, err := tp.ReadLine()
+		if err != nil {
+			fmt.Printf("Error reading line\n")
+		}
+		s.recv = append(s.recv, line)
+		b <- line
+		if line == "JOIN #test-channel" {
+			if len(s.to_send) != 0 {
+				conn.Write([]byte(s.to_send))
+			}
+		}
+	}
+}
+
+func (s *server) run(b chan string) {
+	l, err := net.Listen("tcp", s.addr)
+	if err != nil {
+		fmt.Printf("%v\n", err)
+	} else {
+		for {
+			// Signal to bot that the server is ready to start
+			// accepting connections.
+			b <- ""
+			conn, err := l.Accept()
+			if err != nil {
+				fmt.Printf("%v\n", err)
+			} else {
+				go s.handle(conn, b)
+				l.Close()
+				break
+			}
+		}
+	}
+}
+
+func runTest(lines int, to_send string) []string {
+	InitLogging()
+	b := make(chan string)
+	s := mockServer("localhost:5000", to_send)
+	go s.run(b)
+	bot := NewBot()
+	bot.mockConnect(b)
+	go bot.Run()
+	for i := 0; i < lines; i++ {
+		<-b
+	}
+	return s.recv
+}
+
+func assertEqual(t *testing.T, s1 string, s2 string) {
+	if s1 != s2 {
+		t.Errorf("Expecting \"%v\", received: \"%v\"", s2, s1)
+	}
+}
+
+func TestLoadConfig(t *testing.T) {
+	bot := NewBot()
+	assertEqual(t, bot.Server, "localhost")
+	assertEqual(t, bot.Port, "5000")
+	assertEqual(t, bot.Channel, "#test-channel")
+	assertEqual(t, bot.Trac_URL, "https://tahoe-lafs.org/trac/tahoe-lafs/")
+}
+
+func TestAddAction(t *testing.T) {
+	bot := NewBot()
+	if len(bot.actions) != 0 {
+		t.Errorf("New bot initializes with %v actions. Expected is 0.", len(bot.actions))
+	}
+	bot.AddAction(`#(\d+)`, ParseTicket, FetchTickets)
+	if len(bot.actions) != 1 {
+		t.Errorf("AddAction was not successful. Len(bot.actions) == %v", len(bot.actions))
+	}
+}
+
+func TestConnect(t *testing.T) {
+	resp := runTest(3, "")
+	assertEqual(t, resp[0], "USER tally 8 * :tally")
+	assertEqual(t, resp[1], "NICK tally")
+	assertEqual(t, resp[2], "JOIN #test-channel")
+}
+
+func TestPING(t *testing.T) {
+	resp := runTest(4, "PING test_server\n")
+	assertEqual(t, resp[3], "PONG test_server")
+}
+
+func TestTicketResponse(t *testing.T) {
+	out1 := "PRIVMSG #test-channel :#1382 (immutable peer selection refactoring and enhancements)"
+	out2 := "PRIVMSG #test-channel :https://tahoe-lafs.org/trac/tahoe-lafs/ticket/1382"
+	resp := runTest(5, "ticket #1382\n")
+	assertEqual(t, resp[3], out1)
+	assertEqual(t, resp[4], out2)
+}
+
+func TestMultipleTickets(t *testing.T) {
+	out1 := "PRIVMSG #test-channel :#1382 (immutable peer selection refactoring and enhancements)"
+	out2 := "PRIVMSG #test-channel :https://tahoe-lafs.org/trac/tahoe-lafs/ticket/1382"
+	out3 := "PRIVMSG #test-channel :#1057 (Alter mutable files to use servers of happiness)"
+	out4 := "PRIVMSG #test-channel :https://tahoe-lafs.org/trac/tahoe-lafs/ticket/1057"
+	resp := runTest(7, "working on tickets #1382 and #1057\n")
+	assertEqual(t, resp[3], out1)
+	assertEqual(t, resp[4], out2)
+	assertEqual(t, resp[5], out3)
+	assertEqual(t, resp[6], out4)
+}
+
+func TestBadTicketFormat(t *testing.T) {
+	out1 := "PRIVMSG #test-channel :#1382 (immutable peer selection refactoring and enhancements)"
+	out2 := "PRIVMSG #test-channel :https://tahoe-lafs.org/trac/tahoe-lafs/ticket/1382"
+	out3 := "PRIVMSG #test-channel :#1057 (Alter mutable files to use servers of happiness)"
+	out4 := "PRIVMSG #test-channel :https://tahoe-lafs.org/trac/tahoe-lafs/ticket/1057"
+	resp := runTest(7, "working on tickets #1382 #abc #1182ab and #1057\n")
+	assertEqual(t, resp[3], out1)
+	assertEqual(t, resp[4], out2)
+	assertEqual(t, resp[5], out3)
+	assertEqual(t, resp[6], out4)
+}
+
+func TestNonexistantTicket(t *testing.T) {
+	out1 := "PRIVMSG #test-channel :#1382 (immutable peer selection refactoring and enhancements)"
+	out2 := "PRIVMSG #test-channel :https://tahoe-lafs.org/trac/tahoe-lafs/ticket/1382"
+	resp := runTest(5, "working on tickets #1000000000 and #1382\n")
+	assertEqual(t, resp[3], out1)
+	assertEqual(t, resp[4], out2)
+}
+
+func TestIgnoreList(t *testing.T) {
+	out1 := "PRIVMSG #test-channel :#1382 (immutable peer selection refactoring and enhancements)"
+	out2 := "PRIVMSG #test-channel :https://tahoe-lafs.org/trac/tahoe-lafs/ticket/1382"
+	resp := runTest(5, "another-bot: test #13 check...\n I'm working on ticket #1382\n")
+	assertEqual(t, resp[3], out1)
+	assertEqual(t, resp[4], out2)
+}
